9baeb9a8bad9f9934522f247f817390ede7e649d
akintolatechnologies <akintolatechnologies@gmail.com>
2025-09-20 19:10:51 +0100

    Frontend Refactor and Cleaning

--- core/forms.py
+++ core/forms.py
@@ -4,7 +4,7 @@ from .models import Memorial
 class MemorialForm(forms.ModelForm):
     class Meta:
         model = Memorial
-        fields = ['name', 'birth_date', 'death_date', 'photo',  'card_type']
+        fields = ['name',  'card_type', 'birth_date', 'death_date', 'photo']
         widgets = {
             'birth_date': forms.DateInput(attrs={'type': 'date'}),
             'death_date': forms.DateInput(attrs={'type': 'date'}),
--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index' ),
     path('create_memorial/', views.create_memorial, name='create_memorial' ),
+    path('view_memorial/<int:memorial_id>', views.create_memorial, name='view_memorial' ),
 ]
